290e0efbe3ac850be4c1633aa155210658e22840
ArturoParra <72054956+ArturoParra@users.noreply.github.com>
2021-11-01 19:38:09 -0600

    Add files via upload

---
+++ 5IV8_Parra_Rivas_RSA/src/rsa/RSA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import static rsa.RSA_.desencriptarXD;
+
+/**
+ *
+ * 5IV8 Parra Rivas Arturo Gabriel
+ */
+public class RSA {
+
+    public static void main(String[] args) throws IOException {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
